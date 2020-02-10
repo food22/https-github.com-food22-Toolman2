1f1e0a23bb9ad0114055c15558c1178b81962205
food22 <49146854+food22@users.noreply.github.com>
2020-02-10 16:30:28 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,19 @@
-/**
- * MakeCode editor extension for DHT11 and DHT22 humidity/temperature sensors (BETA)
- * by Alan Wang
- */
-//% block="DHT11/DHT22" weight=100 color=#ff8f3f icon="\uf043"
+//% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
+namespace tools {
+    /**
+    * 計算長方形面積，並回傳
+    */
+    //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
+    //% blockGap=2 weight=0 blockExternalInputs=true
+    export function areaOfRectangle(length: number, width:number): number {
+        return length*width
+    }
+    /**
+    * 計算長方形面積，不回傳，只顯示在LED
+    */
+    //% blockId="ledOfRectangle" block="show area of rectangle length %length|width %width"
+    //% blockGap=2 weight=1
+    export function ledOfRectangle(length: number, width:number): void {
+        basic.showNumber(length*width)
+    }
+}
