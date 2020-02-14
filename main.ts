590b26ff204cf98ce55171a5cfd7db784c9f930d
food22 <49146854+food22@users.noreply.github.com>
2020-02-14 09:48:08 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,37 @@
 //% weight=0 color=#3CB371 icon="\uf0ad" block="Tools"
 namespace tools {
+    let SignalTime = 0
+    let SignalID = 0
     /**
     * 計算長方形面積，並回傳
     */
     //% blockId="areaOfRectangle" block="area of rectangle length %length|width %width"
-    //% blockGap=2 weight=0 blockExternalInputs=true
-    export function areaOfRectangle(length: number, width:number): number {
-        return length*width
+    //% blockGap=2 weight=0
+    export function areaOfRectangle(SignalGroup: number[]): void {
+        while  (pins.digitalReadPin(DigitalPin.P0) == 1)
+        while (pins.digitalReadPin(DigitalPin.P0) == 0) {
+            SingalTime++
+            if (SignalTime > 3600 && SignalID !=0) {
+                SignalTime = 0
+                SignalID = 0                
+                return
+            }
+        }
+        SignalGroup[num] = SignalTime
+        SignalTime = 0
+        num = 0
+        while (pins.digitalReadPin(DigitalPin.P0) == 1) {
+            SingalTime++
+            if (SignalTime > 3600 && SignalID !=0) {
+                SignalTime = 0
+                SignalID = 0                
+                return
+            }
+        }
+        SignalGroup[num] = SignalTime
+        SignalTime = 0
+        num = 0          
+        }
     }
     /**
     * 計算長方形面積，不回傳，只顯示在LED
